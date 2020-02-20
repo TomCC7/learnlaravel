53c936f5dc521766a3324eef4e45d395f68eb769
TomCC7 <cc20070411@sjtu.edu.cn>
2020-02-20 23:10:14 +0800

    用户退出功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,4 +30,10 @@ class SessionsController extends Controller
 
        return;
     }
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
 }
