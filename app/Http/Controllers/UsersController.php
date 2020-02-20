ec9f4e3d6aee92fcdc792dc101cb44a2c593fc9c
TomCC7 <cc20070411@sjtu.edu.cn>
2020-02-20 21:45:13 +0800

    添加gcrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,8 +11,19 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -36,6 +36,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
     //gravatar 头像系统
     public function gravatar($size = '100')
     {
