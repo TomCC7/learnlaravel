d57984da858b2a19eb02c9640ebb205cd98e61c3
TomCC7 <cc20070411@sjtu.edu.cn>
2020-02-20 22:09:08 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -24,6 +25,13 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
