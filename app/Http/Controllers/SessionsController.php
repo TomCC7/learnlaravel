f70e793997244c056b6a0a4bcff1e21479edaa86
TomCC7 <cc20070411@sjtu.edu.cn>
2020-02-21 21:26:33 +0800

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,13 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    // 只允许没有登录的用户访问
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     public function create()
     {
         return view('sessions.create');
@@ -14,18 +21,19 @@ class SessionsController extends Controller
 
     public function store(Request $request)
     {
-       $credentials = $this->validate($request, [
-           'email' => 'required|email|max:255',
-           'password' => 'required'
-       ]);
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
 
-       if (Auth::attempt($credentials, $request->has('remember'))) {
-           session()->flash('success', '欢迎回来！');
-           return redirect()->route('users.show', [Auth::user()]);
-       } else {
-           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-           return redirect()->back()->withInput();
-       }
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来！');
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
     }
 
     public function destroy()
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,19 @@ use Auth;
 
 class UsersController extends Controller
 {
+    // 中间件检测登录情况
+    // __construct 是 PHP 的构造器方法，当一个类对象被创建之前该方法将会被调用。
+    public function __construct()
+    {
+        //only和except
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     //
     public function create()
     {
@@ -40,11 +53,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
@@ -54,7 +69,7 @@ class UsersController extends Controller
         $data['name'] = $request->name;
         if ($request->password) {
             $data['password'] = bcrypt($request->password);
-         }
+        }
         $user->update($data);
 
         session()->flash('success', '个人资料更新成功！');
